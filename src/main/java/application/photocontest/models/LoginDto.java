4ac0dba750dc1098cd1e649c37c999ab0e80af30
LyubomirDenkov <Lybo270@gmail.com>
2021-03-27 12:12:53 +0200

    LoginDto and RegisterDto implemented

---
+++ src/main/java/application/photocontest/models/LoginDto.java
@@ -0,0 +1,39 @@
+package application.photocontest.models;
+
+import javax.validation.constraints.NotNull;
+
+public class LoginDto {
+
+    private static final String USER_NAME_OR_EMAIL_ERROR_MESSAGE = "Username/Email cant be empty";
+    private static final String PASSWORD_ERROR_MESSAGE = "Password cant be empty";
+
+    @NotNull(message = USER_NAME_OR_EMAIL_ERROR_MESSAGE)
+    private String userNameOrEmail;
+
+    @NotNull(message = PASSWORD_ERROR_MESSAGE)
+    private String password;
+
+    public LoginDto() {
+    }
+
+    public LoginDto(String userNameOrEmail, String password) {
+        this.userNameOrEmail = userNameOrEmail;
+        this.password = password;
+    }
+
+    public String getUserNameOrEmail() {
+        return userNameOrEmail;
+    }
+
+    public void setUserNameOrEmail(String userNameOrEmail) {
+        this.userNameOrEmail = userNameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
