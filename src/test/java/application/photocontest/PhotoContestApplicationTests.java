d10cbeda587d700f19add9cb37435cd1007f5070
LyubomirDenkov <Lybo270@gmail.com>
2021-03-27 10:19:26 +0200

    initial commit

---
+++ src/test/java/application/photocontest/PhotoContestApplicationTests.java
@@ -0,0 +1,13 @@
+package application.photocontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoContestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
