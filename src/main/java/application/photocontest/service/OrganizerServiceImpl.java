9acec89ca1611c8a9213702a50763beaca95be6e
Nikolay Georgiev <nikolaypnc@gmail.com>
2021-04-05 11:33:41 +0300

    Organizer create fixed

--- src/main/java/application/photocontest/service/OrganizerServiceImpl.java
+++ src/main/java/application/photocontest/service/OrganizerServiceImpl.java
@@ -1,14 +1,21 @@
 package application.photocontest.service;
 
 import application.photocontest.enums.UserRoles;
+import application.photocontest.exceptions.DuplicateEntityException;
+import application.photocontest.exceptions.EntityNotFoundException;
+import application.photocontest.exceptions.UnauthorizedOperationException;
 import application.photocontest.models.Organizer;
+import application.photocontest.models.Role;
 import application.photocontest.models.UserCredentials;
 import application.photocontest.repository.contracts.OrganizerRepository;
+import application.photocontest.repository.contracts.UserRepository;
 import application.photocontest.service.contracts.OrganizerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static application.photocontest.service.authorization.AuthorizationHelper.verifyUserHasRoles;
 
@@ -17,10 +24,12 @@ public class OrganizerServiceImpl implements OrganizerService {
 
 
     private final OrganizerRepository organizerRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public OrganizerServiceImpl(OrganizerRepository organizerRepository) {
+    public OrganizerServiceImpl(OrganizerRepository organizerRepository, UserRepository userRepository) {
         this.organizerRepository = organizerRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -48,21 +57,53 @@ public class OrganizerServiceImpl implements OrganizerService {
     public Organizer create(UserCredentials userCredentials, Organizer organizer) {
 
         verifyUserHasRoles(userCredentials, UserRoles.ORGANIZER);
+        boolean isUserNameExist = true;
 
-        return organizerRepository.create(organizer);
+        try {
+            organizerRepository.getByUserName(organizer.getCredentials().getUserName());
+        } catch (EntityNotFoundException e) {
+            isUserNameExist = false;
+        }
+
+        if (isUserNameExist) {
+            throw new DuplicateEntityException("");
+        }
+
+
+       Organizer organizerToCreate = organizerRepository.create(organizer);
+       addRoleToRegisteredOrganizer(organizer);
+       return organizerToCreate;
     }
 
     @Override
     public Organizer update(UserCredentials userCredentials, Organizer organizer) {
-
         verifyUserHasRoles(userCredentials, UserRoles.ORGANIZER);
-        return null;
+
+        if (!userCredentials.getUserName().equalsIgnoreCase(organizer.getCredentials().getUserName())){
+            throw new UnauthorizedOperationException("Organizer can update his profile only by himself.");
+        }
+        return organizerRepository.update(organizer);
+
     }
 
     @Override
     public void delete(UserCredentials userCredentials, int id) {
 
         verifyUserHasRoles(userCredentials, UserRoles.ORGANIZER);
+        organizerRepository.delete(id);
 
+    }
+
+    public void addRoleToRegisteredOrganizer(Organizer organizer) {
+        Role role = userRepository.getRoleByName(UserRoles.ORGANIZER.toString());
+        Set<Role> roles;
+        if (organizer.getCredentials().getRoles() == null) {
+            roles = new HashSet<>();
+        } else {
+            roles = organizer.getCredentials().getRoles();
+        }
+        roles.add(role);
+        organizer.getCredentials().setRoles(roles);
+        organizerRepository.update(organizer);
     }
 }
