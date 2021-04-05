518bbd64450da5a9fe525c2dca601d0c0f6e19e6
Nikolay Georgiev <nikolaypnc@gmail.com>
2021-04-05 10:38:02 +0300

    Messages into constants

--- src/main/java/application/photocontest/service/ContestServiceImpl.java
+++ src/main/java/application/photocontest/service/ContestServiceImpl.java
@@ -32,6 +32,20 @@ public class ContestServiceImpl implements ContestService {
     private static final int POINTS_REWARD_WHEN_INVITED_TO_CONTEST = 3;
     private static final int POINTS_REWARD_WHEN_JOINING_OPEN_CONTEST = 1;
     private static final int OPEN = 1;
+    public static final int NEEDED_POINTS_TO_BE_JURY = 150;
+    public static final String RATING_ERROR_MESSAGE = "Only jury can rate images.";
+    public static final String RATING_TWICE_ERROR_MSG = "You cannot rate twice.";
+    public static final String PHASE_RATING_ERROR_MESSAGE = "You can only rate images in second phase.";
+    public static final String RATING_BETWEEN_1_AND_10 = "You can only give points between 1 and 10.";
+    public static final int POINTS_WHEN_PARTICIPANT_ADDED = 1;
+    public static final int MAX_RATING = 10;
+    public static final String CONTEST_INVITATIONAL_ONLY = "Content is invitational only";
+    public static final String CONTEST_PHASE_ERROR_MESSAGE = "You cannot join in a contest which is not in phase one.";
+    public static final String PHOTO_ALREADY_IN_A_CONTEST = "This photo is already in the contest.";
+    public static final String ADD_ONLY_OWN_PHOTOS = "You can add only own photos.";
+    public static final String ONLY_A_PARTICIPANT_CAN_UPLOAD_PHOTO = "Only a participant can upload photo.";
+    public static final String ADDING_IMAGES_ONLY_IN_PHASE_ONE = "You can add photos only in phase one.";
+    public static final String UPDATING_CONTEST_ERROR_MESSAGE = "Only the organizer of the contest can update it.";
 
 
     private final ContestRepository contestRepository;
@@ -140,7 +154,7 @@ public class ContestServiceImpl implements ContestService {
 
 
         if (!organizer.getCredentials().getUserName().equals(contest.getOrganizer().getCredentials().getUserName())) {
-            throw new UnauthorizedOperationException("something");
+            throw new UnauthorizedOperationException(UPDATING_CONTEST_ERROR_MESSAGE);
         }
         setContestJury(contestDto, contest);
         updateParticipants(contest, contestDto);
@@ -199,17 +213,17 @@ public class ContestServiceImpl implements ContestService {
 
         Contest contest = contestRepository.getById(contestId);
         if (!contest.getPhase().getName().equalsIgnoreCase(CONTEST_PHASE_PREPARING)) {
-            throw new UnauthorizedOperationException("You can add photos only in phase one.");
+            throw new UnauthorizedOperationException(ADDING_IMAGES_ONLY_IN_PHASE_ONE);
         }
         if (!contest.getParticipants().contains(userRepository.getUserByUserName(userCredentials.getUserName()))) {
-            throw new UnauthorizedOperationException("Only a participant can upload photo.");
+            throw new UnauthorizedOperationException(ONLY_A_PARTICIPANT_CAN_UPLOAD_PHOTO);
         }
         if (!userRepository.getUserByPictureId(imageId).getUserCredentials().getUserName().equalsIgnoreCase(userCredentials.getUserName())) {
-            throw new UnauthorizedOperationException("You can add only own photos.");
+            throw new UnauthorizedOperationException(ADD_ONLY_OWN_PHOTOS);
         }
 
         if (contest.getImages().contains(imageRepository.getById(imageId))) {
-            throw new UnauthorizedOperationException("This photo is already in the contest.");
+            throw new UnauthorizedOperationException(PHOTO_ALREADY_IN_A_CONTEST);
         }
 
 
@@ -240,10 +254,10 @@ public class ContestServiceImpl implements ContestService {
         Type contestType = contestRepository.getTypeById(OPEN);
 
         if (!contest.getPhase().equals(contestPhase)) {
-            throw new UnauthorizedOperationException("You cannot join in a contest which is not in phase one.");
+            throw new UnauthorizedOperationException(CONTEST_PHASE_ERROR_MESSAGE);
         }
         if (contest.getType().equals(contestType)) {
-            throw new UnauthorizedOperationException("Content is invitational only");
+            throw new UnauthorizedOperationException(CONTEST_INVITATIONAL_ONLY);
         }
 
         Set<User> participants = contest.getParticipants();
@@ -269,14 +283,14 @@ public class ContestServiceImpl implements ContestService {
 
     private Contest checkPointsContestAndImage(int points, int contestId, int imageId) {
 
-        if (points < 1 || points > 10) {
-            throw new IllegalArgumentException("You can only give points between 1 and 10.");
+        if (points < POINTS_WHEN_PARTICIPANT_ADDED || points > MAX_RATING) {
+            throw new IllegalArgumentException(RATING_BETWEEN_1_AND_10);
         }
 
         Contest contest = contestRepository.getById(contestId);
 
         if (!contest.getPhase().getName().equalsIgnoreCase("phaseTwo")) {
-            throw new UnauthorizedOperationException("You can only rate images in second phase.");
+            throw new UnauthorizedOperationException(PHASE_RATING_ERROR_MESSAGE);
         }
 
         Image currentImage = imageRepository.getById(imageId);
@@ -295,7 +309,7 @@ public class ContestServiceImpl implements ContestService {
 
         for (ImageRating imageRating : imageRatings) {
             if (imageRating.getImageId() == imageId) {
-                throw new UnauthorizedOperationException("You cannot rate twice.");
+                throw new UnauthorizedOperationException(RATING_TWICE_ERROR_MSG);
             }
         }
 
@@ -315,7 +329,7 @@ public class ContestServiceImpl implements ContestService {
             user = userRepository.getUserByUserName(userCredentials.getUserName());
 
             if (!contest.getJury().contains(user)) {
-                throw new UnauthorizedOperationException("Only jury can rate images.");
+                throw new UnauthorizedOperationException(RATING_ERROR_MESSAGE);
 
             }
         }
@@ -343,7 +357,7 @@ public class ContestServiceImpl implements ContestService {
 
         for (Integer userId : contestDto.getJury()) {
             User userToAdd = userRepository.getById(userId);
-            if (userToAdd.getPoints() > 150) {
+            if (userToAdd.getPoints() > NEEDED_POINTS_TO_BE_JURY) {
                 jury.add(userToAdd);
             }
         }
