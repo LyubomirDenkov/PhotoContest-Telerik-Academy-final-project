7f7518f6e5417bbcc59a99213541c2c81a586d66
LyubomirDenkov <Lybo270@gmail.com>
2021-04-05 20:28:23 +0300

    index.html restructured , css files formatting code

--- src/main/java/application/photocontest/controllers/mvc/HomeMvcController.java
+++ src/main/java/application/photocontest/controllers/mvc/HomeMvcController.java
@@ -1,7 +1,10 @@
 package application.photocontest.controllers.mvc;
 
-import application.photocontest.models.Image;
+import application.photocontest.controllers.authentications.AuthenticationHelper;
+import application.photocontest.exceptions.UnauthorizedOperationException;
 import application.photocontest.models.User;
+import application.photocontest.models.UserCredentials;
+import application.photocontest.service.contracts.ContestService;
 import application.photocontest.service.contracts.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,28 +12,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Base64;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/")
 public class HomeMvcController {
 
     private final ImageService imageService;
+    private final ContestService contestService;
+    private final AuthenticationHelper authenticationHelper;
 
 
     @Autowired
-    public HomeMvcController(ImageService imageService) {
+    public HomeMvcController(ImageService imageService, ContestService contestService, AuthenticationHelper authenticationHelper) {
         this.imageService = imageService;
+        this.contestService = contestService;
+        this.authenticationHelper = authenticationHelper;
     }
 
 
     @GetMapping
-    public String homePage(Model model) {
+    public String showHomePage(Model model, HttpSession session){
 
-        User user = new User();
-        //Image image = imageService.getById(user,2);
+        UserCredentials currentUser = null;
+        try {
+            currentUser = authenticationHelper.tryGetUser(session);
+            model.addAttribute("currentUser", currentUser);
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("currentUser", currentUser);
+        }
 
-        //model.addAttribute("images", image.getImageData());
+        model.addAttribute("contests", contestService.getAll(currentUser));
+
 
         return "index";
     }
