3be74bfa14be205f5c85f430eefe64a7ea036f58
LyubomirDenkov <Lybo270@gmail.com>
2021-04-06 20:41:13 +0300

    Database changes

--- database/createDatabase.sql
+++ database/createDatabase.sql
@@ -22,6 +22,21 @@ create or replace table contest_type
     name    varchar(20) not null
 );
 
+create or replace table images
+(
+    image_id int auto_increment
+        primary key,
+    title    varchar(50) not null,
+    story    text        not null,
+    image    text        not null
+);
+
+create or replace table points
+(
+    points_id int not null,
+    points    int not null
+);
+
 create or replace table roles
 (
     role_id int auto_increment
@@ -40,14 +55,15 @@ create or replace table user_credentials
 alter table user_credentials
     add primary key (user_name);
 
-create or replace table organizers
+create or replace table users
 (
-    organizer_id     int auto_increment
+    user_id          int auto_increment
         primary key,
     user_credentials varchar(30) not null,
     first_name       varchar(20) not null,
     last_name        varchar(20) not null,
-    constraint organizers_user_credentials_fk
+    profileImage     text        null,
+    constraint users_user_credentials_fk
         foreign key (user_credentials) references user_credentials (user_name)
 );
 
@@ -60,7 +76,7 @@ create or replace table contest
     starting_date   timestamp default current_timestamp() not null on update current_timestamp(),
     phase1_days     int                                   not null,
     phase2_hours    int                                   not null,
-    organizer       int                                   not null,
+    user_id         int                                   not null,
     type_id         int                                   not null,
     image_url       text                                  not null,
     phase_id        int                                   not null,
@@ -68,55 +84,11 @@ create or replace table contest
     constraint contest_category_fk
         foreign key (category_id) references category (category_id),
     constraint contest_organizers_fk
-        foreign key (organizer) references organizers (organizer_id),
+        foreign key (user_id) references users (user_id),
     constraint contest_phase_fk
         foreign key (phase_id) references contest_phase (phase_id),
     constraint contest_type_fk
         foreign key (type_id) references contest_type (type_id)
-);
-
-create or replace table jury_organizers
-(
-    contest_id   int not null,
-    organizer_id int not null,
-    constraint jury_organizers_contest_fk
-        foreign key (contest_id) references contest (contest_id),
-    constraint jury_organizers_fk
-        foreign key (organizer_id) references organizers (organizer_id)
-);
-
-create or replace table users
-(
-    user_id          int auto_increment
-        primary key,
-    user_credentials varchar(30) not null,
-    first_name       varchar(20) not null,
-    last_name        varchar(20) not null,
-    points           int         not null,
-    constraint users_user_credentials_fk
-        foreign key (user_credentials) references user_credentials (user_name)
-);
-
-create or replace table contest_participants
-(
-    contest_id int not null,
-    user_id    int not null,
-    constraint contest_users_contest_fk
-        foreign key (contest_id) references contest (contest_id),
-    constraint contest_users_users_fk
-        foreign key (user_id) references users (user_id)
-);
-
-create or replace table images
-(
-    image_id int auto_increment
-        primary key,
-    title    varchar(50) not null,
-    story    text        not null,
-    image    text        not null,
-    user_id  int         not null,
-    constraint images_users_fk
-        foreign key (user_id) references users (user_id)
 );
 
 create or replace table contest_image
@@ -127,6 +99,16 @@ create or replace table contest_image
         foreign key (contest_id) references contest (contest_id),
     constraint contest_photos_images_fk
         foreign key (image_id) references images (image_id)
+);
+
+create or replace table contest_participants
+(
+    contest_id int not null,
+    user_id    int not null,
+    constraint contest_users_contest_fk
+        foreign key (contest_id) references contest (contest_id),
+    constraint contest_users_users_fk
+        foreign key (user_id) references users (user_id)
 );
 
 create or replace table jury_users
@@ -143,34 +125,35 @@ create or replace table rated_images
 (
     rated_image_id int auto_increment
         primary key,
-    user_name      varchar(30) not null,
-    image_id       int         not null,
-    points         int         not null,
-    comment        longtext    not null,
+    user_id        int      not null,
+    image_id       int      not null,
+    points         int      not null,
+    comment        longtext not null,
     constraint rated_images_images_fk
         foreign key (image_id) references images (image_id),
     constraint rated_images_user_credentials_fk
-        foreign key (user_name) references user_credentials (user_name)
+        foreign key (user_id) references users (user_id)
 );
 
 create or replace table users_images
 (
-    user_credentials varchar(30) not null,
-    image_id         int         not null,
+    user_id  int not null,
+    image_id int not null,
     constraint users_images_images_fk
         foreign key (image_id) references images (image_id),
     constraint users_images_user_credentials_fk
-        foreign key (user_credentials) references user_credentials (user_name)
+        foreign key (user_id) references users (user_id)
 );
 
 create or replace table users_roles
 (
-    user_credentials varchar(30) not null,
-    role_id          int         not null,
+    user_id int not null,
+    role_id int not null,
     constraint users_roles_fk
         foreign key (role_id) references roles (role_id),
     constraint users_roles_user_credentials_fk
-        foreign key (user_credentials) references user_credentials (user_name)
+        foreign key (user_id) references users (user_id)
 );
+
 
 
--- database/insertionInDatabase.sql
+++ database/insertionInDatabase.sql
@@ -28,37 +28,27 @@ VALUES ('nikolay9','12345678'),
        ('ioan123','12345678'),
        ('koki123','12345678');
 
-INSERT INTO users( user_credentials,first_name, last_name, points)
-VALUES ('toshkata',  'Todor', 'Andonov',  200),
-       ('vladi',  'Vladimir', 'Venkov',   1),
-       ('nikolaicho', 'Nikolay', 'Georgiev',   1),
-       ('Peshkata',  'Petar', 'Raykov',  1),
-       ('vankata123',  'Ivancho', 'Ivanov',  9),
-       ('gero99',  'Gero', 'Ivanov',  10),
-       ('pesho_loshiq',  'Petar', 'Petrov',  11),
-       ('lerso1',  'Lerso', 'Kaloqnov', 2),
-       ('kalincho',  'Kalin', 'Kirov',  4),
-       ('ioan123',  'Ioan', 'Naumov',  1),
-       ('koki123',  'Kaloqn', 'Terziev',  1);
+INSERT INTO users( user_credentials,first_name, last_name)
+VALUES ('toshkata',  'Todor', 'Andonov'),
+       ('vladi',  'Vladimir', 'Venkov'),
+       ('nikolaicho', 'Nikolay', 'Georgiev'),
+       ('Peshkata',  'Petar', 'Raykov'),
+       ('vankata123',  'Ivancho', 'Ivanov'),
+       ('gero99',  'Gero', 'Ivanov'),
+       ('pesho_loshiq',  'Petar', 'Petrov'),
+       ('lerso1',  'Lerso', 'Kaloqnov'),
+       ('kalincho',  'Kalin', 'Kirov'),
+       ('ioan123',  'Ioan', 'Naumov'),
+       ('koki123',  'Kaloqn', 'Terziev');
 
-INSERT INTO organizers(user_credentials, first_name, last_name)
-VALUES ('nikolay9', 'Nikolay', 'Georgiev'),
-       ('lybo270',  'Lyubomir', 'Denkov');
-
-INSERT INTO users_roles(user_credentials, role_id)
-VALUES ('nikolay9',2),
-       ('lybo270',2),
-       ('toshkata',1),
-       ('vladi',1),
-       ('nikolaicho',1),
-       ('Peshkata',1),
-       ('vankata123',1),
-       ('gero99',1),
-       ('pesho_loshiq',1),
-       ('lerso1',1),
-       ('kalincho',1),
-       ('ioan123',1),
-       ('koki123',1);
+INSERT INTO users_roles(user_id, role_id)
+VALUES
+(1,2),
+(2,2),
+(3,1),
+(4,1),
+(5,1),
+(6,1);
 
 INSERT INTO category(name)
 VALUES ('Nature'),
@@ -74,13 +64,13 @@ VALUES ('nature');
 INSERT INTO contest_type(name)
 VALUES ('open');
 
-INSERT INTO images (title, story, image, user_id, points) VALUES
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',1,50),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',2,34),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',2,82),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',3,80),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',1,59),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',4,90),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',1,30),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',2,70),
-('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png',1,60);
+INSERT INTO images (title, story, image) VALUES
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png'),
+('how are you doing ','how are you doing ','https://i.imgur.com/JD4Auj5.png');
