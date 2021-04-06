60240700372349fed3642a6285edd55bf9040e24
Nikolay Georgiev <nikolaypnc@gmail.com>
2021-04-06 13:02:13 +0300

    getAll categories refactored html and css

--- src/main/java/application/photocontest/controllers/mvc/ContestsMvcController.java
+++ src/main/java/application/photocontest/controllers/mvc/ContestsMvcController.java
@@ -54,4 +54,6 @@ public class ContestsMvcController {
             return "not-found";
         }
     }
+
+
 }
--- src/main/java/application/photocontest/controllers/mvc/HomeMvcController.java
+++ src/main/java/application/photocontest/controllers/mvc/HomeMvcController.java
@@ -48,4 +48,6 @@ public class HomeMvcController {
         return "index";
     }
 
+
+
 }
