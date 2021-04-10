90f1e1c91efb5d2c106f98da1d68831bd1bb7d91
Nikolay Georgiev <nikolaypnc@gmail.com>
2021-04-10 22:06:40 +0300

    rate picture from mvc controller implemented working 100%

--- src/main/java/application/photocontest/modelmappers/ImageReviewMapper.java
+++ src/main/java/application/photocontest/modelmappers/ImageReviewMapper.java
@@ -1,9 +1,10 @@
 package application.photocontest.modelmappers;
-
 import application.photocontest.models.ImageReview;
 import application.photocontest.models.dto.ImageReviewDto;
 import application.photocontest.repository.contracts.ContestRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ImageReviewMapper {
 
     private final ContestRepository contestRepository;
@@ -12,5 +13,17 @@ public class ImageReviewMapper {
     public ImageReviewMapper(ContestRepository contestRepository) {
         this.contestRepository = contestRepository;
     }
+
+
+
+    public ImageReview fromDto(ImageReviewDto imageReviewDto) {
+        ImageReview imageReview = new ImageReview();
+
+        imageReview.setComment(imageReviewDto.getComment());
+        imageReview.setPoints(imageReviewDto.getPoints());
+
+        return imageReview;
+    }
+
 
 }
