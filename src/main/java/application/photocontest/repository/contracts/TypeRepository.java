aa994da0f7dd2f4eecd259ee3d1fd57b1a0e2e26
Nikolay Georgiev <nikolaypnc@gmail.com>
2021-04-11 14:28:07 +0300

    Generic TypeRepository

--- src/main/java/application/photocontest/repository/contracts/TypeRepository.java
+++ src/main/java/application/photocontest/repository/contracts/TypeRepository.java
@@ -1,12 +1,8 @@
 package application.photocontest.repository.contracts;
 
+
 import application.photocontest.models.Type;
 
-import java.util.List;
+public interface TypeRepository extends GetRepositoryOperations<Type> {
 
-public interface TypeRepository {
-
-    List<Type> getAll();
-
-    Type getById(int id);
 }
