765e90bf755853162986272a5a486b5a3645642c
LyubomirDenkov <Lybo270@gmail.com>
2021-04-19 19:02:07 +0300

    Multiselect checkbox added to contest form

---
+++ src/main/resources/static/multiselect/main.js
@@ -0,0 +1,22 @@
+(function($) {
+
+	"use strict";
+
+	 $(document).ready(function() {
+        $('#multiple-checkboxes').multiselect({
+          includeSelectAllOption: true,
+        });
+    });
+	 
+})(jQuery);
+(function($) {
+
+    "use strict";
+
+    $(document).ready(function() {
+        $('#multiple-checkboxes2').multiselect({
+            includeSelectAllOption: true,
+        });
+    });
+
+})(jQuery);
